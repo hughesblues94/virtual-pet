daae380701e1945ab5dcf275abef364df9fbf54e
hughesblues94 <hughesblues94@gmail.com>
2022-08-13 21:30:59 +0100

    second commit

---
+++ __tests__/pet.test.js
@@ -0,0 +1,12 @@
+const Pet = require('../src/pet');
+
+describe('constructor', () => {
+    test('returns an object', () => {
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+    test('sets the name property', () => {
+        const pet = new Pet("Fido");
+
+        expect(pet.name).toEqual("Fido")
+    })
+});
---
+++ src/pet.js
@@ -0,0 +1,6 @@
+function Pet(name) {
+    this.name = name;
+    return name;
+}
+
+module.exports = Pet;
