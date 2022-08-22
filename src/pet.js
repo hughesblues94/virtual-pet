aca5c2c04baa04333f82090cb69719cf9cb35d8d
hughesblues94 <hughesblues94@gmail.com>
2022-08-22 19:25:49 +0100

    added 'walk' function and set the maximum fitness to 10. Also wrote tests that checked for this functionality

--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,23 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = MAXIMUM_FITNESS;
 }
 
 Pet.prototype.growUp = function () {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
 }
 
+Pet.prototype.walk = function () {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+        this.fitness += 4;
+    } else {
+        this.fitness = 10;
+    }
+}
 module.exports = Pet;
